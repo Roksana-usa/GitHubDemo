00e28ab4f53a87a349026400538b968a25a4f920
Roksana-usa <roksanausabd@gmail.com>
2023-06-21 19:48:15 -0400

    initial commit

---
+++ src/gitHubDemo/HelloGitHub.java
@@ -0,0 +1,12 @@
+package gitHubDemo;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+
+		String toPrint = "I know how to create a separate remote repository in Github and can push my project GithubDemo, without the help of anyone";
+		System.out.println(toPrint);
+
+	}
+
+}
